66c6620c377259051b98eb4c6847cdc26262fd61
Nuhas Masud <nuhasmasud@gmail.com>
2023-07-28 20:17:40 -0400

    Select util, chrome adblock, TopForm Automation done

--- src/main/java/com/qa/automationexercises/factory/DriverFactory.java
+++ src/main/java/com/qa/automationexercises/factory/DriverFactory.java
@@ -1,11 +1,13 @@
 package com.qa.automationexercises.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -23,8 +25,10 @@ public class DriverFactory {
 
 		switch (browserName.toLowerCase().trim()) {
 		case "chrome":
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addExtensions(new File("./src/test/resources/browseraddons/extension_1_51_0_0.crx"));
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(chromeOptions);
 			break;
 		case "firefox":
 			WebDriverManager.firefoxdriver().setup();
